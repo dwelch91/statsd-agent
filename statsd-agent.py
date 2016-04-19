52e76643748e915402d5fb8a08cd508531e13930
Don Welch <dwelch91@gmail.com>
2016-04-19 08:50:38 -0700

    Misc changes
    - Added requirements.txt
    - Added —add-host-field param
    - Simplified/shortened some stat names
    - Add nic= to network measurements

--- statsd-agent.py
+++ statsd-agent.py
@@ -2,11 +2,12 @@ import argparse
 import multiprocessing
 import os
 import platform
+import sys
 import time
+import socket
 
 import psutil
 import statsd
-import sys
 
 system = platform.system()
 isLinux = system == 'Linux'
@@ -26,10 +27,10 @@ def disk(host, port, prefix, basic, fields, interval=10):
 
                 if not basic:
                     counters = psutil.disk_io_counters(False)
-                    pipe.gauge('system_wide.read_time{}'.format(fields), counters.read_time)
-                    pipe.gauge('system_wide.write_time{}'.format(fields), counters.write_time)
+                    pipe.gauge('all.read_time{}'.format(fields), counters.read_time)
+                    pipe.gauge('all.write_time{}'.format(fields), counters.write_time)
                     if isLinux:
-                        pipe.gauge('system_wide.busy_time{}'.format(fields), counters.busy_time)
+                        pipe.gauge('all.busy_time{}'.format(fields), counters.busy_time)
 
             time.sleep(interval)
 
@@ -43,24 +44,24 @@ def cpu_times(host, port, prefix, basic, fields, interval=10):
         while True:
             cpu_times = psutil.cpu_times()
             with client.pipeline() as pipe:
-                pipe.gauge('system_wide.times.user{}'.format(fields), cpu_times.user)
-                pipe.gauge('system_wide.times.system{}'.format(fields), cpu_times.system)
-                pipe.gauge('system_wide.times.idle{}'.format(fields), cpu_times.idle)
+                pipe.gauge('times.user{}'.format(fields), cpu_times.user)
+                pipe.gauge('times.system{}'.format(fields), cpu_times.system)
+                pipe.gauge('times.idle{}'.format(fields), cpu_times.idle)
 
                 if not isWindows:
-                    pipe.gauge('system_wide.times.nice{}'.format(fields), cpu_times.nice)
+                    pipe.gauge('times.nice{}'.format(fields), cpu_times.nice)
                     load = os.getloadavg()
-                    pipe.gauge('system_wide.loadavg.1{}'.format(fields), load[0])
-                    pipe.gauge('system_wide.loadavg.5{}'.format(fields), load[1])
-                    pipe.gauge('system_wide.loadavg.15{}'.format(fields), load[2])
+                    pipe.gauge('loadavg.1{}'.format(fields), load[0])
+                    pipe.gauge('loadavg.5{}'.format(fields), load[1])
+                    pipe.gauge('loadavg.15{}'.format(fields), load[2])
 
                 if isLinux and not basic:
-                    pipe.gauge('system_wide.times.guest_nice{}'.format(fields), cpu_times.guest_nice)
-                    pipe.gauge('system_wide.times.guest{}'.format(fields), cpu_times.guest)
-                    pipe.gauge('system_wide.times.steal{}'.format(fields), cpu_times.steal)
-                    pipe.gauge('system_wide.times.softirq{}'.format(fields), cpu_times.softirq)
-                    pipe.gauge('system_wide.times.iowait{}'.format(fields), cpu_times.iowait)
-                    pipe.gauge('system_wide.times.irq{}'.format(fields), cpu_times.irq)
+                    pipe.gauge('times.guest_nice{}'.format(fields), cpu_times.guest_nice)
+                    pipe.gauge('times.guest{}'.format(fields), cpu_times.guest)
+                    pipe.gauge('times.steal{}'.format(fields), cpu_times.steal)
+                    pipe.gauge('times.softirq{}'.format(fields), cpu_times.softirq)
+                    pipe.gauge('times.iowait{}'.format(fields), cpu_times.iowait)
+                    pipe.gauge('times.irq{}'.format(fields), cpu_times.irq)
 
             time.sleep(interval)
 
@@ -76,21 +77,21 @@ def cpu_times_percent(host, port, prefix, basic, fields, interval=10):
             cpu_times_pcnt = psutil.cpu_times_percent(interval=1)
 
             with client.pipeline() as pipe:
-                pipe.gauge('system_wide.percent{}'.format(fields), value)
-                pipe.gauge('system_wide.times_percent.user{}'.format(fields), cpu_times_pcnt.user)
-                pipe.gauge('system_wide.times_percent.system{}'.format(fields), cpu_times_pcnt.system)
-                pipe.gauge('system_wide.times_percent.idle{}'.format(fields), cpu_times_pcnt.idle)
+                pipe.gauge('percent{}'.format(fields), value)
+                pipe.gauge('percent.user{}'.format(fields), cpu_times_pcnt.user)
+                pipe.gauge('percent.system{}'.format(fields), cpu_times_pcnt.system)
+                pipe.gauge('percent.idle{}'.format(fields), cpu_times_pcnt.idle)
 
                 if not isWindows:
-                    pipe.gauge('system_wide.times_percent.nice{}'.format(fields), cpu_times_pcnt.nice)
+                    pipe.gauge('percent.nice{}'.format(fields), cpu_times_pcnt.nice)
 
                 if isLinux and not basic:
-                    pipe.gauge('system_wide.times_percent.iowait{}'.format(fields), cpu_times_pcnt.iowait)
-                    pipe.gauge('system_wide.times_percent.irq{}'.format(fields), cpu_times_pcnt.irq)
-                    pipe.gauge('system_wide.times_percent.softirq{}'.format(fields), cpu_times_pcnt.softirq)
-                    pipe.gauge('system_wide.times_percent.steal{}'.format(fields), cpu_times_pcnt.steal)
-                    pipe.gauge('system_wide.times_percent.guest{}'.format(fields), cpu_times_pcnt.guest)
-                    pipe.gauge('system_wide.times_percent.guest_nice{}'.format(fields), cpu_times_pcnt.guest_nice)
+                    pipe.gauge('percent.iowait{}'.format(fields), cpu_times_pcnt.iowait)
+                    pipe.gauge('percent.irq{}'.format(fields), cpu_times_pcnt.irq)
+                    pipe.gauge('percent.softirq{}'.format(fields), cpu_times_pcnt.softirq)
+                    pipe.gauge('percent.steal{}'.format(fields), cpu_times_pcnt.steal)
+                    pipe.gauge('percent.guest{}'.format(fields), cpu_times_pcnt.guest)
+                    pipe.gauge('percent.guest_nice{}'.format(fields), cpu_times_pcnt.guest_nice)
 
             time.sleep(interval - 2)
 
@@ -134,7 +135,7 @@ def memory(host, port, prefix, basic, fields, interval=10):
 def network(host, port, prefix, nic, basic, fields, interval=10):
     try:
         client = statsd.StatsClient(host, port, prefix='.'.join([prefix, 'network']))
-
+        fields += ',nic={}'.format(nic)
         prev_bytes_sent, prev_bytes_recv, prev_timer = 0, 0, 0
         while True:
             try:
@@ -154,11 +155,11 @@ def network(host, port, prefix, nic, basic, fields, interval=10):
             prev_timer = timer
 
             with client.pipeline() as pipe:
-                pipe.gauge('network.send_rate{}'.format(fields), send_rate)
-                pipe.gauge('network.recv_rate{}'.format(fields), recv_rate)
+                pipe.gauge('send_rate{}'.format(fields), send_rate)
+                pipe.gauge('recv_rate{}'.format(fields), recv_rate)
                 if not basic:
-                    pipe.gauge('network.send_errors{}'.format(fields), net.errin)
-                    pipe.gauge('network.recv_errors{}'.format(fields), net.errout)
+                    pipe.gauge('send_errors{}'.format(fields), net.errin)
+                    pipe.gauge('recv_errors{}'.format(fields), net.errout)
 
             time.sleep(interval)
 
@@ -170,6 +171,7 @@ def misc(host, port, prefix, _, fields, interval=10):
     try:
         client = statsd.StatsClient(host, port, prefix=prefix)
         boot_time = psutil.boot_time()
+        client.gauge('boot_time{}'.format(fields), boot_time)
         while True:
             with client.pipeline() as pipe:
                 pipe.gauge('uptime{}'.format(fields), time.time() - boot_time)
@@ -187,18 +189,28 @@ if __name__ == '__main__':
     parser.add_argument('--host', '-t', type=str, default='localhost', help='Hostname or IP of statsd server.')
     parser.add_argument('--port', '-p', type=int, default=8125, help='UDP port number of statsd server.')
     parser.add_argument('--prefix', '-x', type=str, default='', help='Prefix value to add to each measurement.')
-    parser.add_argument('--field', '-f', action='append', default=[], help="One or more 'key=value' fields to add to each measurement.")
+    parser.add_argument('--field', '-f', action='append', default=[],
+                        help="One or more 'key=value' fields to add to each measurement.")
     parser.add_argument('--network', '--nic', '-n', type=str,
-                        default='Local Area Connection' if isWindows else 'eth0' if isLinux else 'en0', help='NIC to measure.')
-    parser.add_argument('--basic', '-b', action='store_true', help='If set, only basic measurements gathered and sent to statsd.')
-    parser.add_argument('--interval', '-i', type=int, default=10, help='Time in seconds between measurements. Must be > 2.')
+                        default='Local Area Connection' if isWindows else 'eth0' if isLinux else 'en0',
+                        help='NIC to measure.')
+    parser.add_argument('--basic', '-b', action='store_true',
+                        help='If set, only basic measurements gathered and sent to statsd.')
+    parser.add_argument('--interval', '-i', type=int, default=10,
+                        help='Time in seconds between measurements. Must be > 2.')
+    parser.add_argument('--add-host-field', '-n', action='store_true', help='Auto add host= to fields.')
     args = parser.parse_args()
-    fields = ','.join([f.replace(',', '_').replace(' ', '_').replace('.', '-') for f in args.field])
+    fields = args.field[:]
+
+    if args.add_host_field:
+        fields.append("host={}".format(socket.gethostname()))
+
+    fields = ','.join([f.replace(',', '_').replace(' ', '_').replace('.', '-') for f in fields])
     if fields:
         fields = ',' + fields
 
     if args.interval < 3:
-        print("ERROR: Invalid interval (<3sec).")
+        print("ERROR: Invalid interval (< 3sec).")
         sys.exit(1)
 
     multiprocessing.Process(target=disk, args=(args.host, args.port, args.prefix, args.basic, fields, args.interval)).start()
