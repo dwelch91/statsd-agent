5986ad19b1cbb53d1702624197a0238e04aa3bb9
Don Welch <dwelch91@gmail.com>
2016-04-22 14:56:04 -0700

    one file pyinstaller support

--- statsd-agent.py
+++ statsd-agent.py
@@ -1,5 +1,5 @@
 import argparse
-import multiprocessing
+
 import os
 import platform
 import socket
@@ -17,6 +17,47 @@ import statsd
 system = platform.system()
 isLinux = system == 'Linux'
 isWindows = system == 'Windows'
+
+import os
+import sys
+
+# https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+
+# Module multiprocessing is organized differently in Python 3.4+
+try:
+    # Python 3.4+
+    if isWindows:
+        import multiprocessing.popen_spawn_win32 as forking
+    else:
+        import multiprocessing.popen_fork as forking
+except ImportError:
+    import multiprocessing.forking as forking
+
+if isWindows:
+    # First define a modified version of Popen.
+    class _Popen(forking.Popen):
+        def __init__(self, *args, **kw):
+            if hasattr(sys, 'frozen'):
+                # We have to set original _MEIPASS2 value from sys._MEIPASS
+                # to get --onefile mode working.
+                os.putenv('_MEIPASS2', sys._MEIPASS)
+            try:
+                super(_Popen, self).__init__(*args, **kw)
+            finally:
+                if hasattr(sys, 'frozen'):
+                    # On some platforms (e.g. AIX) 'os.unsetenv()' is not
+                    # available. In those cases we cannot delete the variable
+                    # but only set it to the empty string. The bootloader
+                    # can handle this case.
+                    if hasattr(os, 'unsetenv'):
+                        os.unsetenv('_MEIPASS2')
+                    else:
+                        os.putenv('_MEIPASS2', '')
+
+    # Second override 'Popen' class with our modified version.
+    forking.Popen = _Popen
+
+import multiprocessing
 
 
 def disk(host, port, prefix, basic, fields, interval=10, debug=False):
