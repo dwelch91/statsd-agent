b20188bdf7d9b7cb872490fe8d4a61b6bf2dd2f0
Don Welch <dwelch91@gmail.com>
2016-04-22 14:41:12 -0700

    try to not crash

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup
 import py2exe
 setup(console=['statsd-agent.py'],
-      options={'py2exe': {'bundle_files': 1, 'compressed': True}},
+      options={'py2exe': {'bundle_files': 1, 'compressed': False}},
       zipfile=None)
