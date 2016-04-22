dd22234892c75cfc999554b116e6161f185d84ee
Don Welch <dwelch91@gmail.com>
2016-04-22 14:38:08 -0700

    updated setup

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
 from distutils.core import setup
 import py2exe
-setup(console=['statsd-agent.py'])
+setup(console=['statsd-agent.py'],
+      options={'py2exe': {'bundle_files': 1, 'compressed': True}},
+      zipfile=None)
