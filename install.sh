4f55ca0142c9a957e5f994614584314dd896d6e3
Don Welch <dwelch91@gmail.com>
2016-05-16 10:58:25 -0700

    Verbose

--- install.sh
+++ install.sh
@@ -6,9 +6,12 @@ export TRG_DIR=/opt/statsd-agent
 sudo apt-get install python-pip
 sudo pip install -r requirements.txt
 
-sudo mkdir -p $TRG_DIR
-sudo cp *.py $TRG_DIR
-sudo cp *.cfg $TRG_DIR
-sudo cp *.conf /etc/init
+sudo mkdir -pv $TRG_DIR
+sudo cp -v *.py $TRG_DIR
+sudo cp -v *.cfg $TRG_DIR
+sudo cp -v *.conf /etc/init
 
-echo "TODO: Edit $TRG_DIR/statsd-agent.cfg and fix the service= line."
+echo "TODO: Edit $TRG_DIR/statsd-agent.cfg and fix the service= line and then run:"
+echo "sudo service statsd-agent start"
+echo
+
