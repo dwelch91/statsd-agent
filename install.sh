cba7977d6f55b790bf27bb7d17d731e7a18237d6
Don Welch <dwelch91@gmail.com>
2016-05-17 11:00:06 -0700

    Better logging format and fix to install

--- install.sh
+++ install.sh
@@ -8,10 +8,11 @@ sudo pip install -r requirements.txt
 
 sudo mkdir -pv $TRG_DIR
 sudo cp -v *.py $TRG_DIR
-sudo cp -v *.cfg $TRG_DIR
+sudo cp -v -n *.cfg $TRG_DIR
 sudo cp -v *.conf /etc/init
 
 echo "TODO: Edit $TRG_DIR/statsd-agent.cfg and fix the service= line and then run:"
 echo "sudo service statsd-agent start"
+echo "(Note: If an existing $TRG_DIR/statsd-agent.cfg file was present, this script did not overwrite it.)"
 echo
 
